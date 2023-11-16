d16405ccb27bc18712842a585eaa91e0fcd77ff8
Ayush Gautam <148986884+m-ayush45@users.noreply.github.com>
2023-11-16 08:19:49 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+
+int main() {
+    int units;
+    float rate, total;
+
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    // Define your electricity rate per unit
+    rate = 5.0; // Change this value according to your electricity provider's rate
+
+    if (units <= 50) {
+        total = units * rate;
+    } else if (units <= 100) {
+        total = 50 * rate + (units - 50) * (rate * 1.5);
+    } else if (units <= 200) {
+        total = 50 * rate + 50 * (rate * 1.5) + (units - 100) * (rate * 2);
+    } else {
+        total = 50 * rate + 50 * (rate * 1.5) + 100 * (rate * 2) + (units - 200) * (rate * 2.5);
+    }
+
+    // Add additional charges or taxes if applicable
+    total = total + (total * 0.1); // Example: 10% tax
+
+    printf("Electricity Bill: %.2f\n", total);
+
+    return 0;
+}
